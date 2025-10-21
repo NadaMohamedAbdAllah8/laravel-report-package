abc64f4aee61bae5d7550a612fc19089ce26415c
nadamohamedabdallah <nadamohamedabdallah8@gmail.com>
2025-10-21 08:36:38 +0300

    feat: Adding employee and department

---
+++ app/Models/Employee.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Employee extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'phone',
+        'email',
+        'department_id',
+        'manager_id',
+        'salary',
+        'title',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'salary' => 'decimal:2',
+        ];
+    }
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function manager(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class, 'manager_id');
+    }
+
+    public function subordinates(): HasMany
+    {
+        return $this->hasMany(Employee::class, 'manager_id');
+    }
+}
