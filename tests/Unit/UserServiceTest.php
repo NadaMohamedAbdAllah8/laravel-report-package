5d765750dd3f91da6868650ea40a7ea07aba703d
nadamohamedabdallah <nadamohamedabdallah8@gmail.com>
2025-11-06 14:16:20 +0200

    test: UserService::getOneByEmail

---
+++ tests/Unit/UserServiceTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserServiceTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    private UserService $service;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->service = new UserService;
+    }
+
+    public function test_get_one_by_email_returns_user_when_user(): void
+    {
+        // arrange
+        $email = $this->faker->safeEmail();
+        $fakedUser = User::factory()->create(['email' => $email]);
+
+        // act
+        $user = $this->service->getOneByEmail($email);
+
+        // assert
+        $this->assertNotNull($user);
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertSame($fakedUser->id, $user->id);
+        $this->assertSame($email, $user->email);
+    }
+
+    public function test_get_one_by_email_returns_null_when_not_user(): void
+    {
+        // arrange
+        $userEmail = $this->faker->safeEmail();
+        $randomEmail = $this->faker->safeEmail();
+
+        $fakedUser = User::factory()->create(['email' => $userEmail]);
+
+        // act
+        $user = $this->service->getOneByEmail($randomEmail);
+
+        // assert
+        $this->assertNull($user);
+    }
+}
