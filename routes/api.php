646dd0ceb97c636e52108b125235905f80971175
nadamohamedabdallah <nadamohamedabdallah8@gmail.com>
2025-11-06 16:31:10 +0200

    feat: Department CRUD

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DepartmentController;
 use App\Http\Controllers\Api\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::apiResource('employees', EmployeeController::class);
+Route::apiResource('departments', DepartmentController::class);
