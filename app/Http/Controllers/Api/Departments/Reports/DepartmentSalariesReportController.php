1586b35f95d7527471ff174b486ec61b2270c902
nadamohamedabdallah <nadamohamedabdallah8@gmail.com>
2025-11-26 15:49:12 +0200

    feat: Adding department salaries report

---
+++ app/Http/Controllers/Api/Departments/Reports/DepartmentSalariesReportController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api\Departments\Reports;
+
+use App\Http\Controllers\Controller;
+use App\Services\Departments\DepartmentReportService;
+use App\Traits\RespondsWithJson;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class DepartmentSalariesReportController extends Controller
+{
+    use RespondsWithJson;
+
+    public function __construct(private DepartmentReportService $reports) {}
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        $data = $this->reports->getSalariesReport(filterData: $request->all());
+
+        return $this->returnItemsWithSuccessMessage(
+            items: $data,
+            message: 'Department report retrieved successfully.',
+        );
+    }
+}
--- app/Services/Departments/DepartmentReportService.php
+++ app/Services/Departments/DepartmentReportService.php
@@ -4,13 +4,14 @@ namespace App\Services\Departments;
 
 use App\Models\Department;
 use App\Reports\PaginatedReportBuilder;
+use App\Services\Departments\Dto\DepartmentEmployeeData;
 use Illuminate\Support\Collection;
 
 class DepartmentReportService
 {
     public function getGeneraReport(array $filterData): Collection
     {
-        $employeesCount = function ($employees) {
+        $employeesCount = function ($employees): int {
             return $employees->count();
         };
 
@@ -18,6 +19,24 @@ class DepartmentReportService
             ->attributes(attributes: ['id', 'name'])
             ->relationAttribute(key: 'manager_name', relationAttribute: 'manager.name')
             ->derivedAttribute(key: 'employees_count', lambdaFunction: $employeesCount)
+            ->build()
+            ->get();
+    }
+
+    public function getSalariesReport(array $filterData): Collection
+    {
+        $highestPaidEmployee = function ($employees): DepartmentEmployeeData {
+            return DepartmentEmployeeData::from($employees->sortByDesc('salary')->first());
+        };
+
+        $lowestPaidEmployee = function ($employees): DepartmentEmployeeData {
+            return DepartmentEmployeeData::from($employees->sortBy('salary')->first());
+        };
+
+        return (new PaginatedReportBuilder(query: Department::query()))
+            ->attributes(attributes: ['id', 'name'])
+            ->derivedAttribute(key: 'highest_paid_employee', lambdaFunction: $highestPaidEmployee)
+            ->derivedAttribute(key: 'lowest_paid_employee', lambdaFunction: $lowestPaidEmployee)
             ->build()
             ->get();
     }
